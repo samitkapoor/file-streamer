c18d4428e9b4f06ca6794411afbd98d599a202a2
samitkapoor <samitkapoor77@gmail.com>
2025-07-08 21:36:14 +0530

    pipe json as json

--- src/functions/stream-json.ts
+++ src/functions/stream-json.ts
@@ -86,3 +86,36 @@ export const pipeJsonAsCsv = async (res: Response, data: Data, fileName?: string
     throw err;
   }
 };
+
+export const pipeJsonAsJson = async (res: Response, data: Data, fileName?: string) => {
+  try {
+    let responseFileName = !fileName
+      ? 'data.json'
+      : fileName.endsWith('.json')
+      ? fileName
+      : `${fileName}.json`;
+
+    res.setHeader('Content-Type', 'application/json');
+    res.setHeader('Content-Disposition', `attachment; filename=${responseFileName}`);
+
+    res.write('[\n');
+
+    data.forEach((item: (typeof data)[0], index: number) => {
+      const isLast = index === data.length - 1;
+
+      res.write(JSON.stringify(item, null, 2));
+
+      if (!isLast) {
+        res.write(',\n');
+      } else {
+        res.write('\n');
+      }
+    });
+
+    res.write(']\n');
+    res.end();
+  } catch (err: unknown) {
+    console.error(err);
+    throw err;
+  }
+};
